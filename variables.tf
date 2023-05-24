f7bb9e8e8c179c706702e11c16ace0e00915c44d
Ashay Maheshwari <aaryaashay1848@gmail.com>
2023-05-24 16:02:43 +0530

    Update VM instanceType and DB instance type

--- variables.tf
+++ variables.tf
@@ -5,15 +5,15 @@ variable amiID {
 
 variable instanceType {
     type = string
-   default = "t2.micro"
-#    default = "t2.xlarge"
+#   default = "t2.micro"
+    default = "t2.xlarge"
    
 }
 
 variable dbInstanceType {
     type = string
-    default =  "db.m4.large" 
-#    default = "t2.xlarge"
+#    default =  "db.m4.large" 
+    default = "db.r6g.xlarge"
    
 }
 
